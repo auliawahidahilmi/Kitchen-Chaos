0d6e1ff6fbb2da2e0e7b0d7f5113a901fc6cc6de
auliawahidahilmi <201111039@mhs.stiki.ac.id>
2023-07-22 14:16:07 +0700

    Game Start
    Buat countdown

---
+++ Assets/Scripts/UI/GameStartCountdownUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class GameStartCountdownUI : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI countdownText;
+
+    private void Start()
+    {
+        KitchenGameManager.Instance.OnStateChanged += KitchenGameManager_OnStateChanged;
+
+        Hide();
+    }
+
+    private void KitchenGameManager_OnStateChanged(object sender, EventArgs e)
+    {
+        if (KitchenGameManager.Instance.IsCountdownToStartActive())
+        {
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
+    }
+
+    private void Update()
+    {
+        countdownText.text = Mathf.Ceil(KitchenGameManager.Instance.GetCountdownToStartTimer()).ToString();
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+}
